56cb296a63143997e26cfbd4691559cb881fc8d9
Abigath <162763997+Abigath@users.noreply.github.com>
2024-03-08 19:46:41 -0500

    Create 2_del.bat
    To remove dummycom.

---
+++ 2_del.bat
@@ -0,0 +1,15 @@
+@echo
+del C:\pagefile.pif
+del D:\pagefile.pif
+del E:\pagefile.pif
+del G:\pagefile.pif
+del G:\WINDOWS\system32\Com\smss.exe
+del G:\WINDOWS\system32\Com\lsass.exe
+del G:\WINDOWS\system32\Com\netcfg.000
+del G:\WINDOWS\system32\Com\netcfg.dll
+del G:\WINDOWS\system32\dnsq.dll
+del C:\AUTORUN.INF
+del D:\AUTORUN.INF
+del E:\AUTORUN.INF
+del G:\AUTORUN.INF
+@echo
